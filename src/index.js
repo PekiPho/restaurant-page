f13c0b0d6bb100358af3e416bcaa1a50861805f8
PekiPho <v.a.r.d.games123@gmail.com>
2024-03-03 14:33:59 +0100

    added header animations

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('hello world');
+let HomeBtn=document.querySelector('Home');
+let AboutBtn=document.querySelector('About');
+let ReservationBtn=document.querySelector('Reservations');
+let EventBtn=document.querySelector('Events');
