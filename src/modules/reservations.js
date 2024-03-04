d0933eda95ad7e77132afbe92308b657b043d403
PekiPho <v.a.r.d.games123@gmail.com>
2024-03-04 17:58:42 +0100

    added icons and a logo

--- src/modules/reservations.js
+++ src/modules/reservations.js
@@ -1,3 +1,24 @@
 export default function generateReserve(){
-    console.log('helo');
+    let ReservationBtn=document.querySelector('.Reservations');
+    ReservationBtn.disabled=true;
+    let content=document.querySelector('#content');
+    let container=document.createElement("div");
+    container.classList.add('container');
+    let containerInput=document.createElement('div');
+    containerInput.classList.add('container-input');
+    let inputs=document.createElement('input');
+    let image= new Array(2);
+    for(let i=0;i<2;i++)
+    {
+        image[i]=document.createElement('img');
+        image[i].classList.add('icon');
+    }
+    image[0].src="../assets/phone-in-talk.svg";
+    image[1].src="../assets/map-marker-outline.svg";
+
+
+    container.appendChild(image[0]);
+    container.appendChild(image[1]);
+
+    content.appendChild(container);
 }
