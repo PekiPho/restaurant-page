acde6dc699fc4f233a0293e6ae9674def74a940a
PekiPho <v.a.r.d.games123@gmail.com>
2024-03-06 14:31:07 +0100

    fixed repo, finished reservations

--- src/modules/reservations.js
+++ src/modules/reservations.js
@@ -7,18 +7,35 @@ export default function generateReserve(){
     let containerInput=document.createElement('div');
     containerInput.classList.add('container-input');
     let inputs=document.createElement('input');
+
     let image= new Array(2);
+    let box=new Array(2);
+
+    let phoneTxt=document.createElement('p');
+    phoneTxt.classList.add('restxt');
+    phoneTxt.textContent='+381 123456789';
+
+    let mapTxt=document.createElement('p');
+    mapTxt.classList.add('restxt');
+    mapTxt.textContent='Your Location';
     for(let i=0;i<2;i++)
     {
         image[i]=document.createElement('img');
         image[i].classList.add('icon');
+
+        box[i]=document.createElement('div');
+        box[i].classList.add('box','flex-lay');
+
+        box[i].appendChild(image[i]);
     }
     image[0].src="../assets/phone-in-talk.svg";
     image[1].src="../assets/map-marker-outline.svg";
+    box[0].appendChild(phoneTxt);
+    box[1].appendChild(mapTxt);
 
-
-    container.appendChild(image[0]);
-    container.appendChild(image[1]);
+    container.appendChild(box[0]);
+    container.appendChild(box[1]);
+    container.appendChild(inputs);
 
     content.appendChild(container);
 }
